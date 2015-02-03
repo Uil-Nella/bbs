ed057d741e895e534b68c4cdb40d2a0d032326aa
liuxy <291462491@qq.com>
2015-02-03 20:26:35 +0800

    update url pattern
    Change-Id: I37842e56242a73cc3ba4f64509bba2bdd836748e
    Signed-off-by: liuxy <291462491@qq.com>

--- BBS_Pro/app01/urls.py
+++ BBS_Pro/app01/urls.py
@@ -3,9 +3,7 @@ from django.conf.urls import  url
 import views
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'BBS_Pro.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+   
 
     url(r'^$',views.index),
     url(r'^detail/(\d+)/$',views.bbs_detail),
